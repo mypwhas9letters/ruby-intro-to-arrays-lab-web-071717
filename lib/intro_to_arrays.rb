7c64e730ee16352b4b8158bbceddf2678adeefd5
Hui Wang <huiw3939@gmail.com>
2017-07-07 01:11:15 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  return new_array = []
+end
+
+def array_with_two_elements
+return new_array = [1,2]
+end
+
+def first_element (arr)
+return arr[0]
+end
+
+def third_element (arr)
+  return arr[2]
+end
+
+def last_element (arr)
+  return arr[-1]
+end
+
+def first_element_with_array_methods (arr)
+  return arr.first
+end
+
+def last_element_with_array_methods (arr)
+  return arr.last
+end
+
+def length_of_array (arr)
+  return arr.length
+end
